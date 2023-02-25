fc0ce31c39fd7bf5cc6b8e744519cf6c54c432c2
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2023-02-26 00:12:23 +0600

    Add files via upload

---
+++ Basic Java/Assignment1.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+/* Write a java program that prints firstname ,lastname,gender,Address,balance on your console*/
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("Najmul hassan");
+        System.out.println("GuLla");
+        System.out.println("Male");
+        System.out.println("A/126,Zirani,Ashulia,Dhaka,Bangladesh");
+        System.out.println("Balance");
+
+
+
+
+
+
+    }
+}
